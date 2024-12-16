24269c1707623f18b9663250fada850480921f8c
Gourav kolhatkar <gstar1525@gmail.com>
2024-12-17 00:02:59 +0530

    fixed auth state issue

--- web/src/pages/Auth.tsx
+++ web/src/pages/Auth.tsx
@@ -1,28 +1,28 @@
 import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { useAuth0 } from "@auth0/auth0-react";
+import { useDispatch } from "react-redux";
+import { IdToken, useAuth0 } from "@auth0/auth0-react";
 import { slice as userSlice } from "../state/slices/user";
-import { AppState } from "../state/store";
 
 export function Auth() {
   const { loginWithPopup, getIdTokenClaims } = useAuth0();
   const dispatch = useDispatch();
-  const currentUser = useSelector((state: AppState) => state.currentUser);
 
   useEffect(() => {
-    const authenticateUser = async () => {
-      await loginWithPopup();
-      const claim = await getIdTokenClaims();
-      if (claim) {
-        const idToken = claim.__raw;
-        localStorage.setItem('id_token', idToken)
-        dispatch(userSlice.actions.setCurrentUser(claim));
-      }
-    };
-    if (!currentUser) {
-      authenticateUser();
-    }
+    authenticateUser();
   }, []);
+
+  async function authenticateUser () {
+    let claim = await getIdTokenClaims();
+    if (!claim) {
+      await loginWithPopup();
+      claim = await getIdTokenClaims();
+    }
+    const idToken = claim?.__raw;
+    if (idToken) {
+      localStorage.setItem('id_token', idToken)
+      dispatch(userSlice.actions.setCurrentUser(claim as IdToken));
+    }
+  };
 
   return <h3 className="text-white">LOADING...</h3>;
 }
